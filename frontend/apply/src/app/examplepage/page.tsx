94eeefa45779992ed7bcad1680f72bdea20dd8fe
natta p <natashazaza@live.se>
2025-02-27 14:07:52 +0100

    initialized frontend

---
+++ frontend/apply/src/app/examplepage/page.tsx
@@ -0,0 +1,9 @@
+export default function Examplepage() {
+  return (
+    <div>
+      För att skapa en ny sida skapar man en mapp som heter något, och sedan en
+      fil som heter page.tsx, för att komma till den sidan skriver man sedan i
+      urlen mappens namn, i detta fall "/examplepage"
+    </div>
+  )
+}
