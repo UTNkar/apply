37e39f5c67432b3f495c6ded40d281123a7e92f6
natta p <natashazaza@live.se>
2025-02-27 15:20:38 +0100

    ran npm run format so that eslint github action pr can be merged after this pr

--- frontend/apply/src/app/examplepage/page.tsx
+++ frontend/apply/src/app/examplepage/page.tsx
@@ -3,7 +3,7 @@ export default function Examplepage() {
     <div>
       För att skapa en ny sida skapar man en mapp som heter något, och sedan en
       fil som heter page.tsx, för att komma till den sidan skriver man sedan i
-      urlen mappens namn, i detta fall "/examplepage"
+      urlen mappens namn, i detta fall /examplepage
     </div>
   )
 }
