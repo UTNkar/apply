180d828eac20aeabcd588cd3a27522bff4175265
natta p <natashazaza@live.se>
2025-03-24 20:16:40 +0100

    have set up docker

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # apply
 System for applying for positions within UTN
+
+
+
+
+
+## Docker
+Notera att du måste ha .env filen i root mappen för att allt ska fungera!
+
+# För att starta Docker
+docker-compose up --build  # Bygger och startar alla tjänster
+
+# För att starta om Docker
+docker-compose restart  # Startar om alla tjänster
+
+# För att stoppa och ta bort Docker-containrar
+docker-compose down  # Stoppar och tar bort containrar och nätverk
+
+# För att också ta bort volymer (t.ex. om du vill återställa databasen)
+docker-compose down --volumes  # Stoppar och tar bort containrar, nätverk och volymer
--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'apply.urls'
+ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
     {
@@ -67,19 +68,23 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'apply.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Detta har ändrats sedan standard installationen
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
