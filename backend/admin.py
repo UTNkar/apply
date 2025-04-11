7cbb7466be484b9ddc554d8b8d6e9052b458321e
Freez1t <160607472+Freez1t@users.noreply.github.com>
2025-04-11 06:41:39 +0200

    new member model

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Member
 
 # Register your models here.
+class MemberAdmin(UserAdmin):
+    """
+    Custom admin interface for the Member model.
+    Required as we are using ssn as the username instead of a username
+    and do not want a username field
+    """
+
+    model = Member
+    list_display = ('ssn', 'email', 'name', 'is_staff', 'is_active', 'verified_email')
+    search_fields = ('ssn','email', 'name')
+    ordering = ('ssn','email',)
+    fieldsets = (
+        (None, {'fields': ('ssn','email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'verified_email')}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('ssn', 'email', 'password', 'is_active', 'is_staff', 'is_superuser', 'verified_email')}),
+    )
+
+admin.site.register(Member, MemberAdmin)
