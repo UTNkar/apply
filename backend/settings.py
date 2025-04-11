fca6e046b8e585177982f2ca90ef744f3535f781
Freez1t <160607472+Freez1t@users.noreply.github.com>
2025-04-11 06:27:05 +0200

    settings for authentication

--- backend/settings.py
+++ backend/settings.py
@@ -38,18 +38,46 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'corsheaders',
     'backend'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# CORS
+CORS_ALLOW_CREDENTIALS = True
+if DEBUG:
+    CORS_ALLOWED_ORIGINS = [
+        # based on nextjs dev default
+        "http://localhost:3000",
+    ]
+
+# CSRF 
+# TODO non-debug mode
+if DEBUG:
+    CSRF_COOKIE_DOMAIN = ".localhost"
+    CSRF_TRUSTED_ORIGINS = [
+        # based on nextjs dev default
+        "http://localhost:3000",
+    ]
+
 
 ROOT_URLCONF = 'backend.urls'
 
