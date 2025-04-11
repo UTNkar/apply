9f28e67bccd3aa78a5824189696666a86b3479fd
Freez1t <160607472+Freez1t@users.noreply.github.com>
2025-04-11 17:33:21 +0200

    idk, path works easy, router you have to do something else, should be improved in the future

--- backend/urls.py
+++ backend/urls.py
@@ -36,18 +36,16 @@ router = DefaultRouter()
 
 router.register('positions', PositionViewSet)
 
-router.register('auth/login', LoginAPIView, basename='login')
-router.register('auth/logout', LogoutAPIView, basename='logout')
-router.register('auth/signup', SignupAPIView, basename='signup')
-router.register('auth/verify-email', EmailVerificationAPIView, basename='verify-email')
-router.register('auth/resend-verification-email', ResendVerificationEmailAPIView, basename='resend-verification-email')
-router.register('auth/password-reset', InitiatePasswordResetViewAPIView, basename='password-reset')
-router.register('auth/reset-password', PasswordResetAPIView, basename='reset-password')
-router.register('auth/change-password', ChangePasswordAPIView, basename='change-password')
-router.register('auth/change-email', ChangeEmailAPIView, basename='change-email')
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/auth/login', LoginAPIView.as_view(), name='login'),
+    path('api/auth/logout', LogoutAPIView.as_view(), name='logout'),
+    path('api/auth/signup', SignupAPIView.as_view(), name='signup'),
+    path('api/auth/verify-email', EmailVerificationAPIView.as_view(), name='verify-email'),
+    path('api/auth/resend-verification-email', ResendVerificationEmailAPIView.as_view(), name='resend-verification-email'),
+    path('api/auth/reset-password', InitiatePasswordResetViewAPIView.as_view(), name='reset-password'),
+    path('api/auth/reset', PasswordResetAPIView.as_view(), name='reset'),
+    path('api/auth/change-password', ChangePasswordAPIView.as_view(), name='change-password'),
+    path('api/auth/change-email', ChangeEmailAPIView.as_view(), name='change-email'),
 ]
