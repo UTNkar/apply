8857b2c1afbe14e20b8b85381d9b6b4b9dd63ca7
Ebin Bellini <bengans.polare@gmail.com>
2025-12-22 01:21:21 +0100

    Format 2

--- frontend/apply/src/app/account/page.tsx
+++ frontend/apply/src/app/account/page.tsx
@@ -68,7 +68,7 @@ export default function Account() {
         ...prevState,
         ...data,
       }));
-  });
+  };
 
   useEffect(() => {
     request(Method.GET, "/sections/").then(async (res) => {
