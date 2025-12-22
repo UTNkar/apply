0862757a77e216ccb49219e32e636a262d133f96
Ebin Bellini <bengans.polare@gmail.com>
2025-12-23 00:26:57 +0100

    Fully working login page
    and stylish too!

---
+++ test.py
@@ -0,0 +1,89 @@
+[
+    OrderedDict({
+        'name': 'Göran Vadgörhan',
+        'phone_number': '123-456-7890',
+        'study_program': None,
+        'registration_year': '2020',
+        'status': 'member',
+        'ssn': '199001013333',
+        'email': 'göran@vadgörhan.com',
+        'is_active': True,
+        'is_staff': False,
+        'verified_email': True
+    }),
+    OrderedDict({
+        'name': 'Ebin Bellini',
+        'phone_number': '123-456-7890',
+        'study_program': None,
+        'registration_year': '2020',
+        'status': 'member',
+        'ssn': '199001011337',
+        'email': 'ebin@mejl',
+        'is_active': True,
+        'is_staff': False,
+        'verified_email': False
+    }),
+    OrderedDict({
+        'name': 'Ebin Bellini',
+        'phone_number': '123-456-7890',
+        'study_program': None,
+        'registration_year': '2020',
+        'status': 'member',
+        'ssn': '199001011338',
+        'email': 'ebin@mejl',
+        'is_active': True,
+        'is_staff': False,
+        'verified_email': False
+    }),
+    OrderedDict({
+        'name': 'Ebin Bellini',
+        'phone_number': '123-456-7890',
+        'study_program': None,
+        'registration_year': '2020',
+        'status': 'member',
+        'ssn': '199001011339',
+        'email': 'ebin@mejl.se',
+        'is_active': True,
+        'is_staff': False,
+        'verified_email': False
+    }),
+    OrderedDict({
+        'name': 'Test User',
+        'phone_number': '123-456-7890',
+        'study_program': None,
+        'registration_year': '2020',
+        'status': 'member',
+        'ssn': '199001011234',
+        'email': 'test@example.com',
+        'is_active': True,
+        'is_staff': False,
+        'verified_email': True
+    }),
+    OrderedDict({
+        'name':
+        'Ebin Bellini',
+        'phone_number':
+        '123-456-7893',
+        'study_program':
+        OrderedDict({
+            'id': 3,
+            'name_en': "Master's Programme in Electrical Engineering",
+            'name_sv': "Master's Programme in Electrical Engineering",
+            'section': 2
+        }),
+        'registration_year':
+        '2020',
+        'status':
+        'member',
+        'ssn':
+        '199001011340',
+        'email':
+        'ebinering@mejl.com',
+        'is_active':
+        True,
+        'is_staff':
+        False,
+        'verified_email':
+        True
+    })
+]
