b6462c8f6377b5d1abc14538e4d08262fb883ffb
Ebin Bellini <bengans.polare@gmail.com>
2025-12-23 15:42:29 +0100

    Fix eslint errors for login page

--- frontend/apply/src/app/components/Navbar.tsx
+++ frontend/apply/src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 import styles from '@/styles/navbar.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 import { useState } from 'react'
 import { usePathname } from 'next/navigation'
 import { useIsLoggedIn } from '@/utils/auth';
@@ -13,7 +14,7 @@ const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <a className={styles.logo} href="https://www.utn.se" target="_blank" rel="noopener noreferrer">
-        <img src='/utn_standard_bla.png' alt='Logo' width={200} />
+        <Image src='/utn_standard_bla.png' alt='Logo' width={200} />
       </a>
       <div className={styles.navbarItems}>
         <Link
@@ -32,7 +33,7 @@ const Navbar = () => {
         >
           About
         </Link>
-        {isLoggedIn ? (
+        {loading || isLoggedIn ? (
           <Link
             href='/account'
             className={`${styles.navLink} ${
