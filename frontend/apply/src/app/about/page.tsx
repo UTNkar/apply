d8330397cf1d0c18226935569a17d19bf28b014b
Ebin Bellini <bengans.polare@gmail.com>
2026-03-06 01:58:35 +0100

    About page translation
    Moved text on about page into translation constants and added english translations

--- frontend/apply/src/app/about/page.tsx
+++ frontend/apply/src/app/about/page.tsx
@@ -1,74 +1,43 @@
+"use client";
+
+import { useTranslation } from "react-i18next";
+
 export default function About() {
+  const { t } = useTranslation();
   return (
     <div className="pageContainer">
-      <h1 style={{ marginTop: 48 }}>Att engagera sig i UTN</h1>
-      <p>
-        Att vara engagerad i kåren är den kvintessentiella
-        studentlivsupplevelsen. Man får vara del av att skapa och förbättra
-        upplevelsen för andra studenter, samtidigt som man får så otroligt
-        mycket tillbaka.
-      </p>
+      <h1 style={{ marginTop: 48 }}>{t("aboutPage.title")}</h1>
+      <p>{t("aboutPage.introText")}</p>
       <img src="/städfestival-ht24.jpg" style={{ margin: "12px 0" }}></img>
 
+      <p>{t("aboutPage.timeRequiredText")}</p>
+
+      <p>{t("aboutPage.purposeText")}</p>
+
+      <p>{t("aboutPage.benefitsText")}</p>
+
+      <h2>{t("aboutPage.howToApplyTitle")}</h2>
       <p>
-        Tiden som krävs för att engagera sig kan vara väldigt varierande, allt
-        från att vara volontär några enstaka timmar vid ett tillfälle, till att
-        vara en del av en arbetsgrupp eller kommitté som ses någon/några
-        gång/gånger i veckan under en längre tid, till att arbeta heltid med
-        arvode på kontor.
+        {t("aboutPage.applyHereText")}{" "}
+        <a href="https://apply.utn.se">apply.utn.se</a>!
+      </p>
+
+      <p>{t("aboutPage.applyProcessText")}</p>
+
+      <h2>{t("aboutPage.processingTitle")}</h2>
+      <p>{t("aboutPage.processingText")}</p>
+
+      <h2>{t("aboutPage.questionsTitle")}</h2>
+      <p>
+        {t("aboutPage.moreInfoText")}{" "}
+        <a href={t("aboutPage.moreInfoUrl")}>
+          {t("aboutPage.moreInfoWebsite")}
+        </a>
+        {t("aboutPage.moreInfoContinuationText")}
       </p>
 
       <p>
-        Även syftet med engagemangen är olika. man kan vara del av kårens
-        huvudsyfte&#8212;studiebevakning, där man får chansen att
-        representera studenternas röst i något universitetsorgan. Du kan också
-        vara del av en kommitté som arbetar för att genomföra ett större event,
-        exempelvis Forsränningen eller en mottagning.
-      </p>
-
-      <p>
-        Varje engagemang i UTN erbjuder unika erfarenheter och möjligheter att
-        växa, lära och bidra. Utöver de förmåner man får ta del av under den tid
-        man engagerar sig, har man chansen att utvecklas som person, bygga
-        vänskaper med andra studenter som också vill påverka, och se till att
-        sätta sin prägel på den studiemiljö som vi så länge byggt tillsammans.
-      </p>
-
-      <h2>
-        Hur ansöker jag?
-      </h2>
-      <p>
-        Du söker här på <a href="https://apply.utn.se">apply.utn.se</a>!
-      </p>
-
-      <p>
-        När en post öppnar för ansökningar dyker de upp här. Alla som är
-        medlemmar i Uppsala teknolog- och naturvetarkår får ansöka. Håll koll
-        här och i UTN:s kanaler för information om när sökperioder är öppna!
-      </p>
-
-      <h2>
-        Beredningsprocessen
-      </h2>
-      <p>
-        Urvalsprocessen börjar med en utlysning av lediga poster, där alla
-        intresserade medlemmar är välkomna att ansöka. Efter ansökningsperiodens
-        slut kontaktas oftast de sökande för intervjuer för posten. Vilka det är
-        som håller i intervjun kan variera, men oftast är det kårens
-        valberedning, styrelse och/eller ansvarig instans för posten du har sökt
-        som håller i den.
-      </p>
-
-      <h2>
-        Har du frågor om engagemang?
-      </h2>
-      <p>
-        För att läsa mer, kolla in <a href="https://utn.se/engagera-dig/att-engagera-sig">hemsidan</a>, där vi också har svar på vanliga
-        frågor om att engagera sig.
-      </p>
-
-      <p>
-        Om du undrar något kan du också maila till{" "}
+        {t("aboutPage.contactText")}{" "}
         <a href="mailto:jagvillengageramig@utn.se">jagvillengageramig@utn.se</a>
       </p>
     </div>
