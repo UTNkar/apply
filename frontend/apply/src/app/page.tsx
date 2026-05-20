92abe3238a37debbbd9238b7d94352ece0eda3eb
Ebin Bellini <bengans.polare@gmail.com>
2026-05-20 20:48:26 +0200

    Removed forced loading time

--- frontend/apply/src/app/page.tsx
+++ frontend/apply/src/app/page.tsx
@@ -68,8 +68,7 @@ export default function Home() {
           setApplicationsError("homePage.failedToLoadApplications");
         }
       } finally {
-        setTimeout(() => setLoading(false), 50000); // JUST TESTING
-        //setLoading(false);
+        setLoading(false);
       }
     };
 
